e0f45b8139c517206503340a30b66b31d83b6069
Giuseppe Di Bella <gdibella@amelco.co.uk>
2015-11-13 17:43:33 +0000

    Skeleton Ready

--- src/main/java/com/dibeksupa/supamkt/domain/Item.java
+++ src/main/java/com/dibeksupa/supamkt/domain/Item.java
@@ -3,8 +3,75 @@ package com.dibeksupa.supamkt.domain;
 /**
  * Created by gdibella on 13/11/15.
  */
-public interface Item {
+public class Item implements Item {
 
-    public boolean isDiscount();
-    public String getName();
+    private Long id;
+    private String name;
+    private long  price;
+    private boolean discount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public boolean isDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item that = (Item) o;
+
+        if (price != that.price) return false;
+        if (discount != that.discount) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        return !(name != null ? !name.equals(that.name) : that.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (int) (price ^ (price >>> 32));
+        result = 31 * result + (discount ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", discount=" + discount +
+                '}';
+    }
 }
